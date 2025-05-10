ef084c3dbecf1c09e227defbf036803ee3329676
Taniya220122710 <149950033+Taniya220122710@users.noreply.github.com>
2025-05-11 00:44:17 +0530

    Create ContactForm.jsx

---
+++ ContactForm.jsx
@@ -0,0 +1,11 @@
+<form>
+  <input type="text" placeholder="Your Name" required />
+  <input type="email" placeholder="Email" required />
+  <select>
+    <option>Windows</option>
+    <option>Adobe</option>
+    <option>Autodesk</option>
+  </select>
+  <textarea placeholder="Message"></textarea>
+  <button type="submit">Submit</button>
+</form>
